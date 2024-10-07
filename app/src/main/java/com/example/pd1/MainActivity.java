6921494d0344e82cee23d7e1453f4d78d98c2fa5
ignelitv <igne.litvaityte@knf.stud.vu.lt>
2024-10-07 20:37:51 +0300

    Added change text functionality

--- app/src/main/java/com/example/pd1/MainActivity.java
+++ app/src/main/java/com/example/pd1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pd1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvChangeText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final Button button = findViewById(R.id.btnChangeText);
+        tvChangeText = findViewById(R.id.tvChangeText);
+        button.setOnClickListener(v -> tvChangeText.setText("Welcome to android"));
+
     }
 }
